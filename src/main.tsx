e11c9de4c2346915f4aad117419adc759520ecb8
evripant <evripidispantelaios@gmail.com>
2022-11-05 01:39:16 +0200

    feat: add error-boundary component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
 import App from './App';
+import { ErrorBoundary } from './components/error-boundary/error-boundary';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ErrorBoundary>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ErrorBoundary>
 );
