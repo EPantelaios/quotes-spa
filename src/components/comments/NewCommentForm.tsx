9ed94ee1e69a1babd3cee33a76365aa8ffd2e320
evripant <evripidispantelaios@gmail.com>
2022-11-06 01:22:31 +0200

    feat: add support for comments dynamically

--- src/components/comments/NewCommentForm.tsx
+++ src/components/comments/NewCommentForm.tsx
@@ -1,19 +1,52 @@
-import { useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
+import useHttp from '../../hooks/use-http';
+import { addComment } from '../../lib/api';
+import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './NewCommentForm.module.css';
 
-const NewCommentForm = () => {
+type Props = {
+  quoteId: string;
+  onAddedComment: () => void;
+};
+
+const NewCommentForm = (props: Props) => {
   const commentTextRef = useRef<HTMLTextAreaElement>(null);
+  const [inputComment, setInputComment] = useState<string>('');
+
+  const { sendRequest, status, error } = useHttp(addComment);
+
+  const { onAddedComment } = props;
+
+  useEffect(() => {
+    if (status === 'completed' && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
 
   const submitFormHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    const enteredText = commentTextRef.current?.value;
+    sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
+    setInputComment('');
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === 'pending' && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control}>
         <label htmlFor="comment">Your Comment</label>
-        <textarea id="comment" rows={5} ref={commentTextRef}></textarea>
+        <textarea
+          id="comment"
+          rows={5}
+          ref={commentTextRef}
+          value={inputComment}
+          onChange={(e) => setInputComment(e.target.value)}
+        ></textarea>
       </div>
       <div className={classes.actions}>
         <button className="btn">Add Comment</button>
