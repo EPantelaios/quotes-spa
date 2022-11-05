2a5a9718f951e86118a8a4ffd9ff2ee9ebc0dad1
evripant <evripidispantelaios@gmail.com>
2022-11-05 19:00:07 +0200

    feat: add sorting order button for quotes

--- src/components/quotes/QuoteList.tsx
+++ src/components/quotes/QuoteList.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
@@ -7,18 +8,51 @@ type Props = {
   quotes: { id: string; author: string; text: string }[];
 };
 
+const sortQuotes = (quotes: Props['quotes'], ascending: boolean) => {
+  return quotes.sort((quoteA: { id: string }, quoteB: { id: string }) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props: Props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get('sort') === 'asc';
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const changeSortingHandler = () => {
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? 'desc' : 'asc'}`,
+    });
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>
+          Sort {isSortingAscending ? 'Descending' : 'Ascending'}
+        </button>
+      </div>
       <ul className={classes.list}>
-        {props.quotes.map((quote) => (
-          <QuoteItem
-            key={quote.id}
-            id={quote.id}
-            author={quote.author}
-            text={quote.text}
-          />
-        ))}
+        {sortedQuotes.map(
+          (quote: { id: string; author: string; text: string }) => (
+            <QuoteItem
+              key={quote.id}
+              id={quote.id}
+              author={quote.author}
+              text={quote.text}
+            />
+          )
+        )}
       </ul>
     </Fragment>
   );
